0aa1e5e6eeb5a92beb14a7925f1d842d00bbc4f6
Cristóvão Brandão <128480998+CrisSoBased@users.noreply.github.com>
2024-05-03 20:15:23 +0100

    teste

--- api/index.py
+++ api/index.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://p4:p4@localhost:15432/p4'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Inicialização do SQLAlchemy
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def home():
@@ -12,4 +20,11 @@ def about():
 
 @app.route('/teste')
 def teste():
-    return 'teste a dar'
+    try:
+        # Esta operação é apenas uma verificação de conexão
+        db.session.execute("SELECT 1")
+        connection_message = "Conexão com o banco de dados estabelecida com sucesso!"
+    except Exception as e:
+        connection_message = f"Falha ao conectar ao banco de dados: {str(e)}"
+
+    return connection_message
