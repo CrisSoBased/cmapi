8400d6b6ee73d0aa14a7acf336b81e4db93e9122
Cristóvão Brandão <128480998+CrisSoBased@users.noreply.github.com>
2024-05-03 20:53:02 +0100

    teste

--- api/index.py
+++ api/index.py
@@ -1,14 +1,25 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'cristovao.portugalinteractivo.com'
+app.config['MYSQL_USER'] = 'cristovao_bd'
+app.config['MYSQL_PASSWORD'] = 'B6teCbBcemmw'
+app.config['MYSQL_DB'] = 'cristovao_bd'
 
+mysql = MySQL(app)
 
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+      # Exemplo de consulta ao banco de dados
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM cliente")
+    data = cur.fetchall()
+    cur.close()
+    return str(data)
+
 
 @app.route('/about')
 def about():
