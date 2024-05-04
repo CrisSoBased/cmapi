40aa95a94dd64aa1b4296d7ac3617802eb02d3dd
Cristóvão Brandão <128480998+CrisSoBased@users.noreply.github.com>
2024-05-04 18:51:04 +0100

    teste

--- api/index.py
+++ api/index.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, jsonify
+from datetime import datetime
 import pymysql
 import hashlib
+import random
+import string
+
+
+
 
 app = Flask(__name__)
 
@@ -45,19 +51,61 @@ def newuser():
     email = user_data.get('email')
     password = user_data.get('password')
 
+    # Gerar um token aleatório de 12 caracteres
+    token = ''.join(random.choices(string.ascii_letters + string.digits, k=12))
+
     # Criptografa a senha em MD5
     encrypted_password = hashlib.md5(password.encode()).hexdigest()
+
+    # Criptografa o token em MD5
+    encrypted_token = hashlib.md5(token.encode()).hexdigest()
 
     # Insere os dados do novo usuário na tabela Users
     cursor = conn.cursor()
     try:
-        cursor.execute("INSERT INTO Users (nome, email, password) VALUES (%s, %s, %s)", (nome, email, encrypted_password))
+        cursor.execute("INSERT INTO Users (nome, email, password, token) VALUES (%s, %s, %s, %s)", (nome, email, encrypted_password, encrypted_token))
         conn.commit()
         cursor.close()
-        return jsonify({"message": "Usuário inserido com sucesso!"}), 200
+        return jsonify({"message": "Usuário inserido com sucesso!", "token": token}), 200
     except Exception as e:
         conn.rollback()
         cursor.close()
         return jsonify({"message": "Erro ao inserir usuário: " + str(e)}), 500
 
+
+@app.route('/loginft', methods=['POST'])
+def loginft():
+    # Recebe o JSON com os dados de login
+    login_data = request.json
+    email = login_data.get('email')
+    password = login_data.get('password')
+
+    # Criptografa a senha em MD5
+    encrypted_password = hashlib.md5(password.encode()).hexdigest()
+
+    # Consulta o banco de dados para verificar as credenciais
+    cursor = conn.cursor()
+    cursor.execute("SELECT token, data_token FROM Users WHERE email = %s AND password = %s", (email, encrypted_password))
+    user = cursor.fetchone()
+    cursor.close()
+
+    if user is None:
+        return jsonify({"message": "Credenciais inválidas"}), 401
+
+    token, data_token = user
+
+    # Se o data_token estiver vazio, atualiza-o com a data atual
+    if not data_token:
+        cursor = conn.cursor()
+        try:
+            cursor.execute("UPDATE Users SET data_token = %s WHERE email = %s", (datetime.now(), email))
+            conn.commit()
+            cursor.close()
+        except Exception as e:
+            conn.rollback()
+            cursor.close()
+            return jsonify({"message": "Erro ao atualizar data_token: " + str(e)}), 500
+
+    return jsonify({"token": token}), 200
+    
     
