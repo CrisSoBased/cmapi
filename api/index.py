95df8a64ad1b09ffb028b368968d71d43545b062
B4stos <hdsbastos.99@gmail.com>
2025-06-09 11:58:04 +0100

    userprojects upd 3

--- api/index.py
+++ api/index.py
@@ -247,7 +247,7 @@ def get_user_projects(current_user):
             FROM Projects p
             JOIN UserProjects up ON p.UniqueID = up.project_id
             WHERE up.user_id = %s
-        """, (current_user[0],))  # <- Make sure you're passing only the user ID
+        """, (current_user[0],))  # <-- Make sure you're using current_user[0] here
         results = cursor.fetchall()
         projects = [{"id": row[0], "name": row[1]} for row in results]
         return jsonify(projects), 200
@@ -255,6 +255,8 @@ def get_user_projects(current_user):
         return jsonify({"error": str(e)}), 500
     finally:
         cursor.close()
+
+
 
 
 
